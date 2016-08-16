67eed0a39f606fdf981ead5a9c152fb51cd787b0
wuhaiwei <wuhaiwei0809@163.com>
2016-08-16 14:17:05 +0800

    Initial Commit

---
+++ Test/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Test
+//
+//  Created by  wuhiwi on 16/8/16.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Test/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  Test
+//
+//  Created by  wuhiwi on 16/8/16.
+//  Copyright © 2016年 wanglibank. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
